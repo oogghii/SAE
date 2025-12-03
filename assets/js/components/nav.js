8fd9f763e637d843cec216c0f37a15bbe75b0617
oogghi <raphael.haziza22@gmail.com>
2025-12-03 14:34:53 +0100

    ajout du local storage pour la nav bar

---
+++ assets/js/components/nav.js
@@ -0,0 +1,36 @@
+connectContainer = document.getElementById('connect-container');
+accountContainer = document.getElementById('account-container');
+enqueteContainer = document.getElementById('enquete-container');
+analyseContainer = document.getElementById('analyse-container');
+
+document.addEventListener('DOMContentLoaded', () => {
+    localStorage.setItem('connexionStatus', 'admin');
+    //localStorage.removeItem('connexionStatus');
+
+    const connexionStatus = localStorage.getItem('connexionStatus');
+
+    connectButton(connexionStatus);
+    analyseButton(connexionStatus);
+});
+
+function connectButton(connexionStatus){
+    if (connexionStatus === null) {
+        connectContainer.classList.remove('hidden');
+        accountContainer.classList.add('hidden');
+    }
+    else {
+        connectContainer.classList.add('hidden');
+        accountContainer.classList.remove('hidden');
+    }
+}
+
+function analyseButton(connexionStatus){
+    if (connexionStatus === 'admin') {
+        enqueteContainer.classList.add('hidden');
+        analyseContainer.classList.remove('hidden');
+    }
+    else {
+        enqueteContainer.classList.remove('hidden');
+        analyseContainer.classList.add('hidden');
+    }
+}
